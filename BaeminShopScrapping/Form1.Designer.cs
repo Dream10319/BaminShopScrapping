e76cf194f4c1b0f0ba138659da2fd32e3f4923b9
DreamHigh <lovetom.pro@gmail.com>
2025-02-06 03:58:00 +0800

    Add project files.

---
+++ BaeminShopScrapping/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace BaeminShopScrapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Lon = new System.Windows.Forms.TextBox();
+            this.LocationNum = new System.Windows.Forms.Label();
+            this.Category = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.ShopCounter = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CategoryUpdown = new System.Windows.Forms.NumericUpDown();
+            this.OffsetUpdown = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoryUpdown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OffsetUpdown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(78, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 143);
+            this.progressBar1.Maximum = 10000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(352, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Latitude";
+            // 
+            // Lat
+            // 
+            this.Lat.Location = new System.Drawing.Point(63, 58);
+            this.Lat.Name = "Lat";
+            this.Lat.Size = new System.Drawing.Size(100, 20);
+            this.Lat.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Longitude";
+            // 
+            // Lon
+            // 
+            this.Lon.Location = new System.Drawing.Point(264, 58);
+            this.Lon.Name = "Lon";
+            this.Lon.Size = new System.Drawing.Size(100, 20);
+            this.Lon.TabIndex = 6;
+            // 
+            // LocationNum
+            // 
+            this.LocationNum.AutoSize = true;
+            this.LocationNum.Location = new System.Drawing.Point(70, 122);
+            this.LocationNum.Name = "LocationNum";
+            this.LocationNum.Size = new System.Drawing.Size(44, 13);
+            this.LocationNum.TabIndex = 7;
+            this.LocationNum.Text = "location";
+            // 
+            // Category
+            // 
+            this.Category.AutoSize = true;
+            this.Category.Location = new System.Drawing.Point(160, 122);
+            this.Category.Name = "Category";
+            this.Category.Size = new System.Drawing.Size(48, 13);
+            this.Category.TabIndex = 8;
+            this.Category.Text = "category";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 33);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Option";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(200, 10);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Manual";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(48, 10);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FromFile";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // ShopCounter
+            // 
+            this.ShopCounter.AutoSize = true;
+            this.ShopCounter.Location = new System.Drawing.Point(265, 122);
+            this.ShopCounter.Name = "ShopCounter";
+            this.ShopCounter.Size = new System.Drawing.Size(43, 13);
+            this.ShopCounter.TabIndex = 10;
+            this.ShopCounter.Text = "counter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Offset";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(179, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Category Index";
+            // 
+            // CategoryUpdown
+            // 
+            this.CategoryUpdown.Location = new System.Drawing.Point(264, 85);
+            this.CategoryUpdown.Maximum = new decimal(new int[] {
+            14,
+            0,
+            0,
+            0});
+            this.CategoryUpdown.Name = "CategoryUpdown";
+            this.CategoryUpdown.Size = new System.Drawing.Size(100, 20);
+            this.CategoryUpdown.TabIndex = 14;
+            // 
+            // OffsetUpdown
+            // 
+            this.OffsetUpdown.Location = new System.Drawing.Point(63, 84);
+            this.OffsetUpdown.Name = "OffsetUpdown";
+            this.OffsetUpdown.Size = new System.Drawing.Size(100, 20);
+            this.OffsetUpdown.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 219);
+            this.Controls.Add(this.OffsetUpdown);
+            this.Controls.Add(this.CategoryUpdown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ShopCounter);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Category);
+            this.Controls.Add(this.LocationNum);
+            this.Controls.Add(this.Lon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Lat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BaeminShopScrapping";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoryUpdown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OffsetUpdown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Lat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Lon;
+        private System.Windows.Forms.Label LocationNum;
+        private System.Windows.Forms.Label Category;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label ShopCounter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown CategoryUpdown;
+        private System.Windows.Forms.NumericUpDown OffsetUpdown;
+    }
+}
+
---
+++ BaeminShopScrapping/Form1.cs
@@ -0,0 +1,254 @@
+﻿using Nancy.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaeminShopScrapping
+{
+    public partial class Form1 : Form
+    {
+        Thread th = null;
+        bool isStarted = false;
+        int offset = 0;
+        int catindex = 0;
+        int subtotal = 0;
+        int shopcountersave = 0;
+        int locationnumsave = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(th != null)
+            th.Abort();
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                offset = (int)OffsetUpdown.Value;
+                catindex = (int)CategoryUpdown.Value;
+                if(!isStarted)
+                {
+                    button1.Text = "Pause";
+                    isStarted = true;
+                }
+                else
+                {
+                    button1.Text = "Start";
+                    isStarted = false;
+                    if (th != null)
+                        th.Abort();
+                    return;
+                }
+                th = new Thread(new ThreadStart(() =>
+                {
+                    string filePath = @"locationinfo.txt"; // Adjust the path to where your file is stored.
+
+                    // Regular expression to match one or more spaces or tabs
+                    Regex delimiterRegex = new Regex(@"[\s\t]+");
+
+                    // List to hold the coordinate tuples
+                    List<(string Latitude, string Longitude)> coordinates = new List<(string, string)>();
+
+                    // Read the file line by line
+                    if(radioButton1.Checked)
+                    {
+                        try
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                Lat.Enabled = false;
+                                Lon.Enabled = false;
+                            }));
+                            using (StreamReader reader = new StreamReader(filePath))
+                            {
+                                string line;
+                                bool isFirstLine = true; // Variable to skip the header
+
+                                while ((line = reader.ReadLine()) != null)
+                                {
+                                    if (isFirstLine)
+                                    {
+                                        isFirstLine = false; // Skip the first line which is the header
+                                        continue;
+                                    }
+
+                                    // Split the line into latitude and longitude parts using the regex
+                                    string[] parts = delimiterRegex.Split(line.Trim());
+                                    if (parts.Length >= 2)
+                                    {
+                                        // Add the latitude and longitude as a tuple to the list
+                                        coordinates.Add((parts[0], parts[1]));
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Can't find locationinfo.txt file, please check that");
+                        }
+                    }
+                    else if (radioButton2.Checked)
+                    {
+                        coordinates.Add((Lat.Text, Lon.Text));
+                    }
+
+                    string strUrl = string.Format(@"https://shopdp-api.baemin.com/display-groups/BAEMIN?latitude=37.5450159&longitude=127.1368066&sessionId=b4e3292329dfd570f054c8&carrier=302780&site=7jWXRELC2e&dvcid=OPUD6086af457479a7bb&adid=aede849f-5e9c-499f-827f-cb4e5c65d801&deviceModel=SM-G9500&appver=12.23.0&oscd=2&osver=32&dongCode=11140102&zipCode=04522&ActionTrackingKey=Organic");
+                    RestClient client = new RestClient(strUrl);
+                    RestRequest request = new RestRequest();
+                    request.AddHeader("Accept-Encoding", "gzip, deflate");
+                    request.AddHeader("Connection", "Keep-Alive");
+                    request.AddHeader("Host", "shopdp-api.baemin.com");
+                    request.AddHeader("User-Agent", "and1_12.23.0");
+                    request.AddHeader("USER-BAEDAL", "W/OnG34HSvOVmxn4McyeRzEK3Ldc9+ruPokFIKgQcm0zVU8aOlNuihy2TNW+7I7ZBORlK3kvRun7bOtwlyMA9PnUeLy01xw69qCQLwVBmJdm/hJB8mRTF8vkzRUt/1qkIjb9Tto92g2qIH9ldixRCvPKlFkepp+bOCN6lWvdTIvEx8s0W2jVWA4NWbjnwqqLvKR0wjQxP9pPG3heaCdvvA==");
+                    string strReturn = client.ExecuteGet(request).Content;
+                    JavaScriptSerializer jss = new JavaScriptSerializer();
+                    dynamic data = jss.Deserialize<dynamic>(strReturn);
+                    dynamic categories = data["data"]["displayCategories"];
+
+                    // Output the list of coordinates
+                    int locationNum = locationnumsave;
+                    int shopcounter = shopcountersave;
+                    foreach (var (Latitude, Longitude) in coordinates)
+                    {
+                        locationNum++;
+                        this.Invoke(new Action(() =>
+                        {
+                            Lat.Text = Latitude.ToString();
+                            Lon.Text = Longitude.ToString();
+                            LocationNum.Text = "Location" + locationNum.ToString();
+                        }));
+                        for (int l = catindex; l < categories.Count; l++)
+                        {
+                            this.Invoke(new Action(() =>
+                            {
+                                Category.Text = categories[l]["text"].ToString();
+                            }));
+                            int shopcount = 2000;
+                            int totalcount = subtotal;
+                            for (int i = offset; i <= (int)(shopcount / 25); i++)
+                            {
+                                try
+                                {
+                                    strUrl = string.Format(@"https://shopdp-api.baemin.com/v3/BAEMIN/shops?displayCategory={3}&longitude={0}&latitude={1}&sort=SORT__DEFAULT&filter=&offset={2}&limit=25&extension=&perseusSessionId=1718023403008.788454282780365941.FWy8AA9FNv&memberNumber=000000000000&sessionId=b4e3292329dfd570f054c8&carrier=302780&site=7jWXRELC2e&dvcid=OPUD6086af457479a7bb&adid=aede849f-5e9c-499f-827f-cb4e5c65d801&deviceModel=SM-G9500&appver=12.23.0&oscd=2&osver=32&dongCode=11140102&zipCode=04522&ActionTrackingKey=Organic", Longitude.ToString(), Latitude.ToString(), 25 * i, Category.Text);
+                                    File.WriteAllText("log.txt", Environment.NewLine + "lat:" + Lat.Text + ", lon:" + Lon.Text + ", offset:" + i.ToString() + ", catindex:" + l.ToString());
+                                    this.Invoke(new Action(() =>
+                                    {
+                                        CategoryUpdown.Value = l;
+                                        OffsetUpdown.Value = i;
+                                    }));
+                                    client = new RestClient(strUrl);
+                                    strReturn = client.ExecuteGet(request).Content;
+                                    if (strReturn.Contains("SUCCESS"))
+                                    {
+                                        jss = new JavaScriptSerializer();
+                                        data = jss.Deserialize<dynamic>(strReturn);
+                                        dynamic shops = data["data"]["shops"];
+                                        shopcount = (int)data["data"]["totalCount"];
+                                        foreach (var shop in shops)
+                                        {
+                                            totalcount++;
+                                            string shopnumber = shop["shopInfo"]["shopNumber"].ToString();
+                                            if (!string.IsNullOrEmpty(shopnumber))
+                                            {
+                                                try
+                                                {
+                                                    strUrl = string.Format(@"https://shopdp-api.baemin.com/v8/shop/{0}/detail?lat={1}&lng={2}&limit=25&mem=&memid=&defaultreview=N&campaignId=2353465&displayGroup=BAEMIN&lat4Distance=37.5670653&lng4Distance=126.98168738&filter=&sessionId=1447226b282d5e40f677b5a1d37&carrier=302780&site=7jWXRELC2e&dvcid=OPUD6086af457479a7bb&adid=aede849f-5e9c-499f-827f-cb4e5c65d801&deviceModel=SM-G9500&appver=12.23.0&oscd=2&osver=32&dongCode=11140102&zipCode=04522&ActionTrackingKey=Organic", shopnumber, Latitude.ToString(), Longitude.ToString());
+                                                    client = new RestClient(strUrl);
+                                                    strReturn = client.ExecuteGet(request).Content;
+                                                    if (strReturn.Contains("SUCCESS"))
+                                                    {
+                                                        var dir = $"ShopMenus\\{shopnumber}";
+                                                        Directory.CreateDirectory(dir);
+                                                        File.WriteAllText(string.Format(@"{0}\{1}.json", dir, shopnumber), strReturn);
+                                                        if (shop["shopInfo"] != null)
+                                                        File.WriteAllText(string.Format(@"{0}\{1}-logo.json", dir, shopnumber), shop["shopInfo"].ToString());
+                                                        shopcounter++;
+                                                        jss = new JavaScriptSerializer();
+                                                        data = jss.Deserialize<dynamic>(strReturn);
+                                                        dynamic groupMenus = data["data"]["shop_menu"]["menu_ord"]["groupMenus"];
+                                                        foreach(var groupMenu in groupMenus)
+                                                        {
+                                                            if(groupMenu["menus"] != null)
+                                                            foreach (var menu in groupMenu["menus"])
+                                                            {
+                                                                strUrl = string.Format($@"https://shopdp-api.baemin.com/v1/shops/{shopnumber}/menus/{menu["menuId"]}?useDelivery=true&useTakeout=true&useTableOrder=false&sessionId=1bd602fb042e017b54087217&carrier=302780&site=7jWXRELC2e&dvcid=OPUDf48850e556873dfc&adid=4bd027e0-d307-4740-8866-a9e00e4861f1&deviceModel=SM-G9500&appver=12.23.0&oscd=2&osver=32&dongCode=28237101&zipCode=21404&ActionTrackingKey=Organic");
+                                                                client = new RestClient(strUrl);
+                                                                strReturn = client.ExecuteGet(request).Content;
+                                                                File.WriteAllText(string.Format(@"{0}\{1}-{2}.json", dir, shopnumber, menu["menuId"]), strReturn);
+                                                                this.Invoke(new Action(() =>
+                                                                {
+                                                                    progressBar1.Value = (int)((10000 * totalcount) / shopcount);
+                                                                }));
+                                                                this.Invoke(new Action(() =>
+                                                                {
+                                                                    ShopCounter.Text = shopcounter.ToString();
+                                                                }));
+                                                                shopcountersave = shopcounter;
+                                                                subtotal = totalcount;
+                                                            }  
+                                                        }  
+                                                    }
+                                                }
+                                                catch (Exception ex)
+                                                {
+
+                                                }
+
+                                            }
+                                        }
+                                    }
+                                    else
+                                    {
+                                        shopcount = 0;
+                                    }
+
+                                }
+                                catch (Exception ex)
+                                {
+
+                                }
+
+                            }
+                        }
+                        locationnumsave = locationNum;
+
+                    }
+                    if(locationNum > 0)
+                    {
+                        progressBar1.Value = 10000;
+                        MessageBox.Show("Successfully done!!!");
+                    }
+                }));
+                th.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(LocationNum.Text);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            Lat.Enabled = true;
+            Lon.Enabled = true;
+        }
+    }
+}
